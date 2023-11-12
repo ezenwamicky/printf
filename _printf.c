5b93e2508adbaaf848d89509454b211f5edaf2ce
ezenwamicky <ezenwamagnanimous419@gmail.com>
2023-11-12 02:02:59 +0000

    hopefully this works to complete task 0

--- _printf.c
+++ _printf.c
@@ -1,5 +1,43 @@
 #include "main.h"
 /**
+ *handlechar - handles characters variables
+ *@args : variable arguments
+ *@charcount : arguments count variable
+ */
+void handlechar(va_list args, int *charcount)
+{
+	char c = va_arg(args, int);
+
+	putchar(c);
+	(*charcount)++;
+}
+/**
+ *handlestring - handles string of characters variables
+ *@args : variable arguments
+ *@charcount : arguments count variable
+ */
+void handlestring(va_list args, int *charcount)
+{
+	char *str = va_arg(args, char *);
+
+	while (*str != '\0')
+	{
+		putchar(*str);
+		str++;
+		(*charcount)++;
+	}
+}
+/**
+ *handlepercent - handles percrnt placeholder variables
+ *@charcount : arguments count variable
+ */
+void handlepercent(int *charcount)
+{
+	putchar('%');
+	(*charcount)++;
+}
+
+/**
  *_printf - prints formatted output to stdout.
  *@format: Format string containing format specifiers.
  *@...: variable number of arguments corresponding to format specifiers
@@ -10,42 +48,33 @@ int _printf(const char *format, ...)
 	int charcount = 0;
 
 	va_list args;
+
 	va_start(args, format);
 
-
-	while (*format != '\0')
+	for (; *format != '\0'; format++)
 	{
 		if (*format == '%')
 		{
 			format++;
+
 			switch (*format)
 			{
 				case 'c':
-					{
-						char c = va_arg(args, int);
-						putchar(c);
-						charcount++;
-						break;
-					}
+				handlechar(args, &charcount);
+				break;
+
 				case 's':
-					{
-						char *str = va_arg(args, char *);
-						while (*str != '\0')
-						{
-							putchar(*str);
-							str++;
-							charcount++;
-						}
-						break;
-					}
+				handlestring(args, &charcount);
+				break;
+
 				case '%':
-					{
-						putchar('%');
-						charcount++;
-						break;
-					}
+				handlepercent(&charcount);
+				break;
+
 				default:
-					break;
+				putchar(*format);
+				charcount++;
+				break;
 			}
 		}
 		else
@@ -53,9 +82,7 @@ int _printf(const char *format, ...)
 			putchar(*format);
 			charcount++;
 		}
-		format++;
 	}
-
 	va_end(args);
-	return charcount;
+	return (charcount);
 }
